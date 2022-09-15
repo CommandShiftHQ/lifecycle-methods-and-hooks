1d230a5ee63896bf551832bb814dbc3d5a0380f4
StuartCowley <voidstalker@live.co.uk>
2022-09-15 15:09:15 +0100

    add useEffect to get random word and display

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "../styles/app.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [word, setWord] = useState("");
+
+  useEffect(() => {
+    console.log("App mounted");
+    axios.get("https://random-word-api.herokuapp.com/word").then((res) => {
+      setWord(res.data[0]);
+    });
+    return () => {
+      console.log("App unmounted");
+    };
+  }, []);
+
+  useEffect(() =>{
+    console.log("Count updated");
+  }, [count])
 
   const handleClick = () => {
     setCount((prev) => prev + 1);
@@ -14,6 +30,10 @@ const App = () => {
         <div className="app__container-title">useState</div>
         <div>Current count: {count}</div>
         <button onClick={handleClick}>+</button>
+      </div>
+      <div className="app__container">
+        <div className="app__container-title">useEffect</div>
+        <div>Fetched word: {word}</div>
       </div>
     </div>
   );
